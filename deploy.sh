ee03ff396175db6ecf849da03cc56c9e808aa963
壹陣上古風 <1421718760@qq.com>
2020-09-28 18:31:14 +0800

    0.1

--- deploy.sh
+++ deploy.sh
@@ -22,5 +22,4 @@ git push -f git@github.com:technicolortheclown.github.io.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-
 cd -
